3b9e05c44564fc14579dcd5e3f17cc3ee49d050f
Master <master@masters-mac.local>
2016-07-02 21:44:41 +0200

    working 1

---
+++ FireChatSample/LogInVC.swift
@@ -0,0 +1,20 @@
+//
+//  LogInVC.swift
+//  FireChatSample
+//
+//  Created by Master on 7/2/16.
+//  Copyright © 2016 Master. All rights reserved.
+//
+
+import UIKit
+
+class LogInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
